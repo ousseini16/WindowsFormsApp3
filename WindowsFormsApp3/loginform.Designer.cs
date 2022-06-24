f7bfe7a3b9d76b989e1d04879bc4dc87d38e0862
Ousseini Yadiga <ousseinioudo16@gmail.com>
2022-06-25 01:09:39 +0200

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp3/loginform.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class loginform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.usrtxt = new System.Windows.Forms.TextBox();
+            this.passtext = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(99, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Admin_Login";
+            // 
+            // usrtxt
+            // 
+            this.usrtxt.Location = new System.Drawing.Point(238, 122);
+            this.usrtxt.Name = "usrtxt";
+            this.usrtxt.Size = new System.Drawing.Size(344, 22);
+            this.usrtxt.TabIndex = 5;
+            // 
+            // passtext
+            // 
+            this.passtext.Location = new System.Drawing.Point(238, 210);
+            this.passtext.Name = "passtext";
+            this.passtext.Size = new System.Drawing.Size(344, 22);
+            this.passtext.TabIndex = 6;
+            // 
+            // loginform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.passtext);
+            this.Controls.Add(this.usrtxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "loginform";
+            this.Text = "loginform";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox usrtxt;
+        private System.Windows.Forms.TextBox passtext;
+    }
+}
---
+++ WindowsFormsApp3/loginform.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class loginform : Form
+    {
+        public loginform()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bank_dbEntities1 dbe = new bank_dbEntities1();
+            if(usrtxt.Text !=String.Empty || passtext.Text != String.Empty)
+            {
+                var user1 = dbe.Admin_table.FirstOrDefault(a => a.Username.Equals(usrtxt.Text));
+                if(user1 != null)
+                {
+                     if(user1.Password.Equals(passtext.Text))
+                    {
+                        Menu m1 = new Menu();
+                        m1.ShowDialog();
+                    }
+                     else
+                    {
+                        MessageBox.Show("Password incorrect");
+                    }
+
+
+                } else
+                {
+                    MessageBox.Show("null value");
+                }
+
+
+            }  
+            else
+            {
+                MessageBox.Show("entrer le nom d'utilisateur et le moss de pass");
+            }
+        }
+    }
+}
